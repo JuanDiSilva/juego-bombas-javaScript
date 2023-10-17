32b30feb9b1d936bdb258e6f1e80acae5c86e375
JuanDiSilva <silvachaves@unicauca.edu.co>
2023-10-17 14:33:15 -0500

    1.Elavoracion canvas

--- java/game.js
+++ java/game.js
@@ -2,14 +2,18 @@
 const canvas = document.querySelector('#game');
 const game = canvas.getContext('2d');
 
-//Carga de la ventana canvas, inicio juego
-window.addEventListener('load', inicioGame);
+//Constantes let scope
+//Se define una variables let para el tamaño de canvas, size
+let canvasSize;
+let elementosSize;
 
-//Con esta funcion se trabaja sobre el canvas
-function inicioGame()
+//Carga de la ventana canvas, inicio juego
+window.addEventListener('load', colocarCanvasTamanio);
+windows.addEventListener('resize', colocarCanvasTamanio);
+
+//Con esta funcion se cambia el tamanio al canvas
+function colocarCanvasTamanio()
 {
-    //Se define una variable loca para el tamaño de canvas, size
-    let canvasSize;
     if(window.innerWidth > window.innerHeight)
     {
         canvasSize = window.innerHeight * 0.8;
@@ -21,9 +25,14 @@ function inicioGame()
     //Atribotos de la anchura y altura
     canvas.setAttribute('width',canvasSize)
     canvas.setAttribute('height',canvasSize)
-    
     //Se define el bloque para los pixelees de cada emoji, elementos tamaño
-    const elementosSize = canvasSize / 10;
+    elementosSize = canvasSize / 10;
+
+    inicioGame();
+}
+//Funcion para iniciar el juego en canvas
+function inicioGame()
+{
     console.log({ canvasSize,elementosSize });
     
     game.font = elementosSize + 'px Verdana';
@@ -31,16 +40,10 @@ function inicioGame()
 
     for(let i = 1; i <= 10; i++)
     {
-        game.fillText(emojis['X'], elementosSize * i, elementosSize);
+        for(let j =1; j<=10; j++)
+        {
+          //Se coloca +(lementosSize/5) y -(elementosSize/5) para que se adapte a cualquier pantalla
+          game.fillText(emojis['X'], (elementosSize * i)+(elementosSize/5), (elementosSize * j)-(elementosSize/5));
+        }
     }
-    //game.fillRect(0,50,100,100);
-    //game.clearRect(50,50,50,50);
-    //game.clearRect();
-    //game.clearRect(0,0,50,50);
-    
-    //Para que el texto tenga color, tamaño y se vea centrado
-    
-    //game.fillStyle = 'purple';
-    //game.textAlign = 'center';
-    //game.fillText('JulioGame', 80, 50);
 }
